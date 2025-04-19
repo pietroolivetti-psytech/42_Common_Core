2f6c0e29bf05a7b37a21ae811831153b1d7d738a
pietroolivetti-psytech <pietroolivettiradio@gmail.com>
2025-04-19 20:25:27 +0100

    Add files via upload

---
+++ libft_1804/tests/libft_tests.h
@@ -0,0 +1,17 @@
+#ifndef LIBFT_TESTS_H
+# define LIBFT_TESTS_H
+
+# include <stddef.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+#include <string.h>
+
+#define check(condition) \
+    if (condition) { \
+        printf("[OK]✅ Test passed (line %d)\n", __LINE__); \
+    } else { \
+        printf("[FAIL]❌ Test failed (line %d)\n", __LINE__); \
+    }
+
+#endif
